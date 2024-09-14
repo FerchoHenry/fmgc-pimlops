eb5018176061d08707be9b538806c87dd9c021e3
FerchoRack <fcorts@outlook.com>
2024-09-14 00:46:49 -0300

    4 funciones probar en render

--- notebooks/main.py
+++ notebooks/main.py
@@ -13,20 +13,11 @@ app = FastAPI()
 async def root():
     return {"message": "Hola Carola3"}
 
-# @app.get("/items/{item_id}")
-# def read_item(item_id: int, q: Optional[str] = None):
-#     return {"item_id": item_id, "q": q}
-
-# ejemplo: http://127.0.0.1:8000/items/42?q=example
-# respuesta: {"item_id":42,"q":"example"}
-
 #1_ Cantidad de filmaciones mes
 @app.get("/cantidad_filmaciones_mes/{mes}")
-def cantidad_filmaciones_mes(mes: str):
-    # Convertir el nombre del mes a minúsculas
-    mes = mes.lower()
-    
-    # Mapeo de meses en español a números
+def cantidad_filmaciones_mes(mes):
+        
+    # Mapeo de meses
     meses = {
         "enero": 1, "febrero": 2, "marzo": 3, "abril": 4,
         "mayo": 5, "junio": 6, "julio": 7, "agosto": 8,
@@ -40,5 +31,58 @@ def cantidad_filmaciones_mes(mes: str):
     filmaciones_mes = df[df['release_date'].dt.month == mes_numero]
     
     return {
-        "mensaje": f"{len(filmaciones_mes)} películas fueron estrenadas en el mes de {mes.capitalize()}."
+        "mensaje": f"{len(filmaciones_mes)} cantidad de películas fueron estrenadas en el mes de {mes}"
     }
+#2_ Cantidad de filmaciones dia
+    
+@app.get("/cantidad_filmaciones_dia/{dia}")
+def cantidad_filmaciones_dia(dia):
+    
+    # Mapeo de días 
+    dias = {
+        "lunes": 0, "martes": 1, "miércoles": 2, "jueves": 3,
+        "viernes": 4, "sábado": 5, "domingo": 6
+    }
+         
+    dia_numero = dias[dia]
+    
+    # Filtrar por día de estreno
+    df['release_date'] = pd.to_datetime(df['release_date'], errors='coerce')
+    filmaciones_dia = df[df['release_date'].dt.weekday == dia_numero]
+    
+    return {
+        "mensaje": f"{len(filmaciones_dia)} cantidad de películas fueron estrenadas en los días {dia}"
+    }
+3# Score titulo
+
+@app.get("/score_titulo/{titulo}")
+def score_titulo(titulo):
+    # Filtrar el DataFrame por título, paso todo a minusculas para evitar errores.
+    film = df[df['title'].str.lower() == titulo.lower()]
+    
+    if film.empty:
+        return {"error": "Película no encontrada"}
+    
+    #si arroja mas de un resultado me quedo con la primera   
+    film_info = film.iloc[0]
+    
+    return { "mensaje": f"La película {film_info['title']} fue estrenada en el año {film_info['release_year']} con un score/popularidad de {film_info['popularity']}"}
+
+#4 votos titulos
+
+@app.get("/votos_titulo/{titulo}")
+def votos_titulo(titulo: str):
+    # Filtrar el DataFrame por título, paso todo a minusculas para evitar errores.
+    film = df[df['title'].str.lower() == titulo.lower()]
+    
+    if film.empty:
+        return {"error": "Película no encontrada"}
+    
+    #si arroja mas de un resultado me quedo con la primera
+    film_info = film.iloc[0]
+    
+    #if film_info['vote_count'] < 2000:
+    #    return {"mensaje": "La película no cumple con el mínimo de 2000 valoraciones."}
+    
+    return { "mensaje": f" La pelicula {film_info['title']} fue estrenada en el año {film_info['release_year']}. La misma cuenta con un total de agregar film_info['vote_count'] valoraciones, con un promedio de {film_info['vote_average']}"}
+       
