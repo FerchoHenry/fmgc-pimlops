37683e3d44e69b8c1c37fd51ffb5d41cdc222304
FerchoRack <fcorts@outlook.com>
2024-09-14 19:30:08 -0300

    6 endpoints

--- notebooks/main.py
+++ notebooks/main.py
@@ -1,10 +1,15 @@
 #extraido de https://fastapi.tiangolo.com/tutorial/first-steps/
 import pandas as pd
+import numpy as np
 from fastapi import FastAPI
 #from typing import Optional
 
 # Cargar el dataset
-df = pd.read_parquet('../data/movies_etl.parquet')
+df = pd.read_parquet('../data/movies_etl3.parquet')
+dfcredits = pd.read_parquet('../data/credits_etl.parquet')
+
+#pasamos a Str la columna ID para los merges de los punto 5 y 6
+dfcredits['id'] = dfcredits['id'].astype(str)
 
 
 app = FastAPI()
@@ -66,12 +71,12 @@ def score_titulo(titulo):
     #si arroja mas de un resultado me quedo con la primera   
     film_info = film.iloc[0]
     
-    return { "mensaje": f"La película {film_info['title']} fue estrenada en el año {film_info['release_year']} con un score/popularidad de {film_info['popularity']}"}
+    return { "mensaje": f"La película {film_info['title']} fue estrenada en el año {roun(film_info['release_year'])} con un score/popularidad de {film_info['popularity']}"}
 
 #4 votos titulos
 
 @app.get("/votos_titulo/{titulo}")
-def votos_titulo(titulo: str):
+def votos_titulo(titulo):
     # Filtrar el DataFrame por título, paso todo a minusculas para evitar errores.
     film = df[df['title'].str.lower() == titulo.lower()]
     
@@ -81,8 +86,47 @@ def votos_titulo(titulo: str):
     #si arroja mas de un resultado me quedo con la primera
     film_info = film.iloc[0]
     
-    #if film_info['vote_count'] < 2000:
-    #    return {"mensaje": "La película no cumple con el mínimo de 2000 valoraciones."}
+    if film_info['vote_count'] < 2000:
+        return {"mensaje": "La película no cumple con el mínimo de 2000 valoraciones."}
     
-    return { "mensaje": f" La pelicula {film_info['title']} fue estrenada en el año {film_info['release_year']}. La misma cuenta con un total de agregar film_info['vote_count'] valoraciones, con un promedio de {film_info['vote_average']}"}
-       
+    return { "mensaje": f" La pelicula {film_info['title']} fue estrenada en el año {round(film_info['release_year'])}. La misma cuenta con un total de {round(film_info['vote_count'])} valoraciones, con un promedio de {film_info['vote_average']}"}  
+
+#5 get actor
+
+@app.get("/get_actor/{nombre_actor}")
+def get_actor(nombre_actor):
+    # Filtrar el DataFrame por actor
+    films_actor = dfcredits[dfcredits['cast'].str.contains(nombre_actor, case=False, na=False)]
+    
+    if films_actor.empty:
+        return {"error": "Actor no encontrado"}
+    
+    merge_dfrev = pd.merge(df[['id','revenue']], films_actor, on='id', how='right')
+    
+    cantidad_filmaciones = len(films_actor)
+    retorno_total = merge_dfrev['revenue'].sum()
+    promedio_retorno = merge_dfrev['revenue'].mean()
+     
+    return { "mensaje" : f"El actor {nombre_actor} ha participado de {cantidad_filmaciones} cantidad de filmaciones, el mismo ha conseguido un retorno de {retorno_total} con un promedio de {promedio_retorno:.2F} por filmación"}
+
+# 6 get director
+@app.get("/get_director/{nombre_director}")
+def get_director(nombre_director):
+    # Filtrar el DataFrame por director
+    films_director = dfcredits[dfcredits['crew'].str.contains(nombre_director, case=False, na=False)]
+    
+    if films_director.empty:
+        return {"error": "Director no encontrado"}
+    
+    merge_crew =  pd.merge(df[['id','title','budget','release_date','revenue','return']], films_director, on='id', how='right')
+    retorno_total = np.sum(merge_crew['return'][np.isfinite(merge_crew['return'])])  
+    
+    return { "mensaje" : f"el director {nombre_director} tiene un retorno total de: {retorno_total:.2f}" ,
+            "detalle de peliculas": f"{merge_crew}"}  
+             
+# Se ingresa el nombre de un director que se encuentre dentro de un dataset debiendo devolver 
+# el éxito del mismo medido a través del retorno. Además, 
+# deberá devolver el nombre de cada película con la fecha de lanzamiento, retorno individual, costo y ganancia de la misma.
+#exito = retorno
+#nombre,fecha de lanzamiento, retorno individual, costo y ganancia 
+  
